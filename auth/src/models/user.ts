53a5049210a70d453f917ab1c538f2c1cb1579d9
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-06-21 20:57:52 +0100

    feat: User Document interface

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -8,9 +8,16 @@ interface UserAttrs {
 }
 
 // An interface that describes the properties
-// that a User model has
-interface UserModel extends mongoose.Model<any> {
-	build(attrs: UserAttrs): any;
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+	email: string;
+	password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -29,6 +36,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
 	return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
