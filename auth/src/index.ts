8c6ee7e3b3221c940ec89aaf0c4f87749b8cc249
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-06-21 19:56:04 +0100

    feat: connect to mongoDB cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { errHandler } from './middlewares/err-handler';
 import { NotFoundError } from './errs/not-found-err';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -23,6 +24,21 @@ app.all('*', async () => {
 // middlewares
 app.use(errHandler);
 
-app.listen(3000, () => {
-	console.log('Listent on port: 3000');
-});
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('Connected to mongoDB');
+	} catch (e) {
+		console.error(e);
+	}
+
+	app.listen(3000, () => {
+		console.log('Listent on port: 3000');
+	});
+};
+
+start();
