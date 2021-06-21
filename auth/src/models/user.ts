f8ce3fb446f260d6f835046c019375a21b213ec1
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-06-21 20:52:36 +0100

    feat: user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+// An interface that describe properties that are
+// required to create a new User
+interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+// An interface that describes the properties
+// that a User model has
+interface UserModel extends mongoose.Model<any> {
+	build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+	},
+
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+	return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+export { User };
