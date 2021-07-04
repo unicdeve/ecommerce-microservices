f4514ceccd3fb2635714fccc1d4f76bea090657b
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-04 21:42:50 +0100

    feat: currentUser middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/current-user', (req, res) => {
-	if (!req.session?.jwt) return res.send({ currentUser: null });
-
-	try {
-		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-
-		res.send({ currentUser: payload });
-	} catch (e) {
-		res.send({ currentUser: null });
-	}
+router.get('/api/users/current-user', currentUser, (req, res) => {
+	res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
