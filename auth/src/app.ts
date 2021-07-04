cfdaff88f649add1aa4a63ad4aa25776f277ddb3
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-04 22:37:18 +0100

    refacor: create app.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express, { json } from 'express';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/current-user';
+import { signUpRouter } from './routes/signup';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { errHandler } from './middlewares/err-handler';
+import { NotFoundError } from './errs/not-found-err';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true,
+	})
+);
+
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+
+app.all('*', async () => {
+	throw new NotFoundError();
+});
+
+// middlewares
+app.use(errHandler);
+
+export { app };
