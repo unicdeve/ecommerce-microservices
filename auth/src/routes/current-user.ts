b6335aa2e76e4ef4a5a6f17e9cb7ab3437d2485d
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-04 21:23:13 +0100

    feat: get current user handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/current-user', (req, res) => {
-	res.send('Hi Taiwo againjkj');
+	if (!req.session?.jwt) return res.send({ currentUser: null });
+
+	try {
+		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+		res.send({ currentUser: payload });
+	} catch (e) {
+		res.send({ currentUser: null });
+	}
 });
 
 export { router as currentUserRouter };
