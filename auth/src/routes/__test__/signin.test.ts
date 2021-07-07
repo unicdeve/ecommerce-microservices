353ec1b511870073d95bd95372ba4c5f7d5c3c97
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-07 22:37:30 +0100

    test: sign in

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('fails when an email that does not exist is supplied', async () => {
+	await request(app)
+		.post('/api/users/signin')
+		.send({
+			password: 'password',
+			email: 'test@test.org',
+		})
+		.expect(400);
+});
+
+it('fails when an incorrect password is supplied', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			password: 'password',
+			email: 'test@test.org',
+		})
+		.expect(201);
+
+	await request(app)
+		.post('/api/users/signin')
+		.send({
+			password: 'p',
+			email: 'test@test.org',
+		})
+		.expect(400);
+});
+
+it('sets a cookie after successful sign in', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			password: 'password',
+			email: 'test@test.org',
+		})
+		.expect(201);
+
+	const res = await request(app)
+		.post('/api/users/signin')
+		.send({
+			password: 'password',
+			email: 'test@test.org',
+		})
+		.expect(201);
+
+	expect(res.get('Set-Cookie')).toBeDefined();
+});
