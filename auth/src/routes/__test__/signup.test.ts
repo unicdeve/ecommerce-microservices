504bcd645764a0be4eeff3b8919b3cc37da27533
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-07 22:17:48 +0100

    fix: more tests for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,37 @@ it('returns a 201 on successful signup', () => {
 		})
 		.expect(201);
 });
+
+it('returns a 400 with invalid email', () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test.org',
+			password: 'password',
+		})
+		.expect(400);
+});
+
+it('returns a 400 with missing password', () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.org',
+			password: 'p',
+		})
+		.expect(400);
+});
+
+it('returns a 400 with missing email and password', () => {
+	return request(app).post('/api/users/signup').send({}).expect(400);
+});
+
+it('returns a 400 with missing email or password', async () => {
+	await request(app).post('/api/users/signup').send({
+		password: 'password',
+	});
+	await request(app)
+		.post('/api/users/signup')
+		.send({ email: 'test@test.org' })
+		.expect(400);
+});
