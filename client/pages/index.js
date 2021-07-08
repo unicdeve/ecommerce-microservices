b99ada12ab1ead06a263eaf9188f5eb683eae2e2
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-08 22:27:11 +0100

    feat: getInitialProps on server and on browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,44 @@
-export default function Home() {
+import axios from 'axios';
+
+function Home({ currentUser }) {
+	console.log('on browser', currentUser);
+
+	// axios.get('/api/users/current-user').catch((err) => {});
+
 	return (
 		<div>
 			<h1>Home page 00</h1>
 		</div>
 	);
 }
+
+Home.getInitialProps = async () => {
+	if (typeof window === 'undefined') {
+		const { data } = await axios
+			.get(
+				'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user',
+				{
+					headers: {
+						host: 'ticketing.dev',
+					},
+				}
+			)
+			.catch((err) => {
+				console.log(err.response.data);
+			});
+
+		return data;
+	} else {
+		const res = await axios.get('/api/users/current-user').catch((err) => {
+			console.log(err.response.data);
+		});
+
+		return res.data;
+	}
+
+	console.log('Executed!!!');
+
+	return {};
+};
+
+export default Home;
