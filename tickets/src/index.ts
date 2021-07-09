284d5091ce0162be3f086d250d56a40e323e8136
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-09 20:54:04 +0100

    feat: tickets service k8s configs

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,12 +5,12 @@ const start = async () => {
 	if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined!');
 
 	try {
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+		await mongoose.connect('mongodb://tickets-mongo-srv:27017/auth', {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true,
 		});
-		console.log('Connected to mongoDB');
+		console.log('Connected to tickets service mongoDB');
 	} catch (e) {
 		console.error(e);
 	}
