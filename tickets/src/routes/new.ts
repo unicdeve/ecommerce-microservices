cb035c35c3b568bc746624e8676449eeb246b47f
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-11 19:42:40 +0100

    feat: validate title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,20 @@
-import { requireAuth } from '@unicdeve/common';
+import { requireAuth, validateRequest } from '@unicdeve/common';
+import { body } from 'express-validator';
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-	res.sendStatus(200);
-});
+router.post(
+	'/api/tickets',
+	requireAuth,
+	[
+		body('title').not().isEmpty().withMessage('Title is required'),
+		body('price').isFloat({ gt: 0 }).withMessage('Price must be valid'),
+	],
+	validateRequest,
+	(req: Request, res: Response) => {
+		res.sendStatus(200);
+	}
+);
 
 export { router as createTicketRouter };
