563fbc72920610faa4009e17f60650d12bbdd857
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-14 11:10:30 +0100

    feat: setDeliverAllAvailable and setDurableName on client listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,10 +15,15 @@ stan.on('connect', () => {
 		process.exit();
 	});
 
-	const options = stan.subscriptionOptions().setManualAckMode(true);
+	const options = stan
+		.subscriptionOptions()
+		.setManualAckMode(true)
+		.setDeliverAllAvailable()
+		.setDurableName('orders-service');
+
 	const subscription = stan.subscribe(
 		'ticket:created',
-		'orders-service-queue-group',
+		'queue-group-name',
 		options
 	);
 
