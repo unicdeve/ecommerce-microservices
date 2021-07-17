31188a24c0fc891da1aa88a60c6cf6cf6e2dc665
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-18 00:46:43 +0100

    feat: TicketUpdatedEvent

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -9,6 +9,9 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
 
 	onMessage(data: TicketCreatedEvent['data'], msg: Message) {
 		console.log('Event data', data);
+		console.log(data.id);
+		console.log(data.title);
+		console.log(data.price);
 		msg.ack();
 	}
 }
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
 import { body } from 'express-validator';
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -37,6 +39,13 @@ router.put(
 		});
 
 		await ticket.save();
+
+		new TicketUpdatedPublisher(natsWrapper.client).publish({
+			id: ticket.id,
+			title: ticket.title,
+			price: ticket.price,
+			userId: req.currentUser!.id,
+		});
 
 		res.send(ticket);
 	}
