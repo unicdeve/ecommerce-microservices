e1a07c86e5db49aeb65b8e069c4a84cc394e0b29
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-18 19:39:33 +0100

    feat: order service setup

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express, { json } from 'express';
+import 'express-async-errors';
+import { errHandler, NotFoundError, currentUser } from '@unicdeve/common';
+import cookieSession from 'cookie-session';
+// import { createTicketRouter } from './routes/new';
+// import { showTicketRouter } from './routes/show';
+// import { indexTicketRouter } from './routes';
+// import { updateTicketRouter } from './routes/update';
+
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
+
+app.use(currentUser);
+
+// app.use(indexTicketRouter);
+// app.use(createTicketRouter);
+// app.use(showTicketRouter);
+// app.use(updateTicketRouter);
+
+app.all('*', async () => {
+	throw new NotFoundError();
+});
+
+// middlewares
+app.use(errHandler);
+
+export { app };
