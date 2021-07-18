39ff16be30915b95c766fbe03d810044e06dee5b
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-18 21:37:42 +0100

    wip: existingOrder

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { requireAuth, validateRequest } from '@unicdeve/common';
+import {
+	BadRequestError,
+	NotFoundError,
+	OrderStatus,
+	requireAuth,
+	validateRequest,
+} from '@unicdeve/common';
 import { body } from 'express-validator';
 import express, { Request, Response } from 'express';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -10,6 +18,33 @@ router.post(
 	[body('ticketId').not().isEmpty().withMessage('TicketID is required')],
 	validateRequest,
 	async (req: Request, res: Response) => {
+		const { ticketId } = req.body;
+		// Find the ticket the user is trying to purchase
+		const ticket = await Ticket.findById(ticketId);
+		if (!ticket) throw new NotFoundError();
+
+		// Make sure the ticket is not already reserved
+		// Find an order with the same ticket ID and
+		// make sure the order status is NOT cancelled
+		const existingOrder = await Order.findOne({
+			ticket,
+			status: {
+				$in: [
+					OrderStatus.Created,
+					OrderStatus.AwaitingPayment,
+					OrderStatus.Completed,
+				],
+			},
+		});
+
+		if (existingOrder) throw new BadRequestError('Ticket is already reserved.');
+
+		// Calculate an expiration date for this order
+
+		// Build the order and save it to DB
+
+		// TODO: Publish OrderCreated event
+
 		res.status(201).send('new order');
 	}
 );
