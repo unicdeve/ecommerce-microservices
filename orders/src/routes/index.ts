720868305316db844070564156340f23a9ae4152
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-19 16:28:51 +0100

    feat: fetch user's orders

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
+import { requireAuth } from '@unicdeve/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-	res.send('Get orders routes');
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+	const orders = await Order.find({ userId: req.currentUser!.id }).populate(
+		'ticket'
+	);
+
+	res.send(orders);
 });
 
 export { router as indexOrderRouter };
