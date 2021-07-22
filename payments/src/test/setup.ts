9d1d27087cb8791a005ab38ba67cdbd43f262e7a
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-22 23:14:43 +0100

    feat: payments-srv setups

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,54 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+	var signin: (id?: string) => string[];
+}
+
+jest.mock('../nats-wrapper.ts');
+
+let mongo: any;
+beforeAll(async () => {
+	process.env.JWT_KEY = 'snkjnkj';
+	mongo = new MongoMemoryServer();
+	await mongo.start();
+	const mongoUri = mongo.getUri();
+
+	await mongoose.connect(mongoUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	});
+});
+
+beforeEach(async () => {
+	jest.clearAllMocks();
+	const collections = await mongoose.connection.db.collections();
+
+	for (let col of collections) {
+		await col.deleteMany({});
+	}
+});
+
+afterAll(async () => {
+	await mongo.stop();
+});
+
+global.signin = () => {
+	const id = mongoose.Types.ObjectId().toHexString();
+	const payload = {
+		email: 'test@test.com',
+		id,
+	};
+
+	const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+	const session = { jwt: token };
+
+	const sessionJSON = JSON.stringify(session);
+
+	const base64 = Buffer.from(sessionJSON).toString('base64');
+
+	return [`express:sess=${base64}`];
+};
