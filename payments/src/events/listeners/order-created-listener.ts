0b6ad78f2296a362c70b2de9a366651f0263248d
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-23 17:58:18 +0100

    feat OrderCreatedListener for payments-srv

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,36 @@
+import {
+	Listener,
+	NotFoundError,
+	OrderCreatedEvent,
+	Subjects,
+} from '@unicdeve/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+	readonly subject = Subjects.OrderCreated;
+	queueGroupName = queueGroupName;
+
+	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+		const {
+			id,
+			status,
+			userId,
+			version,
+			ticket: { price },
+		} = data;
+
+		const order = Order.build({
+			id,
+			price,
+			status,
+			version,
+			userId,
+		});
+		await order.save();
+
+		// ack the message
+		msg.ack();
+	}
+}
