79961ecb9c6ab1031d76fe1e9f3234b274fb094c
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-23 18:15:19 +0100

    feat: order-cancelled-listener

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,28 @@
+import {
+	Listener,
+	NotFoundError,
+	OrderCancelledEvent,
+	OrderStatus,
+	Subjects,
+} from '@unicdeve/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+	readonly subject = Subjects.OrderCancelled;
+	queueGroupName = queueGroupName;
+
+	async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+		const { id, version } = data;
+
+		const order = await Order.findOne({ _id: id, version });
+
+		if (!order) throw new NotFoundError();
+
+		order.set({ status: OrderStatus.Cancelled });
+		await order.save();
+
+		msg.ack();
+	}
+}
