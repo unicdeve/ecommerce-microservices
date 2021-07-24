8df375850b2fd23a94291505b81040b0e69a12fd
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-24 23:05:04 +0100

    feat: payments validations for an order

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { requireAuth, validateRequest } from '@unicdeve/common';
+import {
+	BadRequestError,
+	NotAuthorizedError,
+	NotFoundError,
+	OrderStatus,
+	requireAuth,
+	validateRequest,
+} from '@unicdeve/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -12,7 +20,18 @@ router.post(
 		body('orderId').not().isEmpty().withMessage('Order ID is required'),
 	],
 	validateRequest,
-	(req: Request, res: Response) => {
+	async (req: Request, res: Response) => {
+		const { token, orderId } = req.body;
+
+		const order = await Order.findById(orderId);
+
+		if (!order) throw new NotFoundError();
+
+		if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+
+		if (order.status === OrderStatus.Cancelled)
+			throw new BadRequestError('Order cancelled already.');
+
 		res.send({ success: true });
 	}
 );
