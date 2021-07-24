600bd00f6ead25b4ea0b4436f6b71aa3d629d3bc
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-25 00:03:49 +0100

    test: stripe charge

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -2,6 +2,9 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order, OrderStatus } from '../../models/order';
+import { stripe } from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('returns a 404 if order does not exist', async () => {
 	await request(app)
@@ -53,4 +56,30 @@ it('returns a 400 if order is cancelled', async () => {
 			orderId: order.id,
 		})
 		.expect(400);
+});
+
+it('returns a 204 with valid inputs', async () => {
+	const userId = mongoose.Types.ObjectId().toHexString();
+	const order = Order.build({
+		id: mongoose.Types.ObjectId().toHexString(),
+		price: 122,
+		userId,
+		version: 0,
+		status: OrderStatus.Created,
+	});
+	await order.save();
+
+	await request(app)
+		.post('/api/payments')
+		.set('Cookie', global.signin(userId))
+		.send({
+			token: 'tok_visa',
+			orderId: order.id,
+		})
+		.expect(201);
+
+	const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+	expect(chargeOptions.source).toEqual('tok_visa');
+	expect(chargeOptions.currency).toEqual('usd');
+	expect(chargeOptions.amount).toEqual(122 * 100);
 });
