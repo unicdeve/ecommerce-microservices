991ccfdcc7ea551d0b52d1c70b67e55ccf0c0cd6
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-07-25 09:15:18 +0100

    feat: create an order

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,7 +1,31 @@
-export default function TicketDetail() {
+import { useRequest } from '../../hooks/use-request';
+
+function TicketDetail({ ticket }) {
+	const { doRequest, errors } = useRequest({
+		url: '/api/orders',
+		method: 'post',
+		initialState: { ticketId: ticket.id },
+		onSuccess: (data) => console.log('order data', data),
+	});
+
 	return (
 		<div>
-			<h1>Ticket details</h1>
+			<h1>{ticket.title}</h1>
+			<p>Price: ${ticket.price}</p>
+			{errors}
+			<button className='btn btn-primary' onClick={async () => doRequest()}>
+				Purchase
+			</button>
 		</div>
 	);
 }
+
+TicketDetail.getInitialProps = async (context, axios) => {
+	const { ticketId } = context.query;
+
+	const { data } = await axios.get(`/api/tickets/${ticketId}`);
+
+	return { ticket: data };
+};
+
+export default TicketDetail;
