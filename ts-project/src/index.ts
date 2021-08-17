7e46180df8f525a400e74ea44fbbdc8f7bdb2546
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-08-17 22:54:58 +0100

    feat: display map

--- ts-project/src/index.ts
+++ ts-project/src/index.ts
@@ -2,7 +2,10 @@
 import { Company } from './Company';
 import { User } from './User';
 
-const user = new User();
-const company = new Company();
-
-console.log(user, company);
+new google.maps.Map(document.getElementById('map'), {
+	zoom: 1,
+	center: {
+		lat: 0,
+		lng: 0,
+	},
+});
